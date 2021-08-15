eaa2aa1e7904c8bc53ff0c3c126a2a2d2d6a39c4
maafigueiredo <maafigueiredo@gmail.com>
2021-08-15 12:13:29 -0300

    "adicionando rotas"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { TransferenciaService } from './services/transferencia.service';
 import { Component } from '@angular/core';
+import { Transferencia } from './models/transferencia.model';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent {
 
   constructor(private service: TransferenciaService) {}
 
-  transferir($event: any) {
+  transferir($event: Transferencia) {
     this.service.adicionar($event).subscribe(x => console.log(x));
   }
 }
